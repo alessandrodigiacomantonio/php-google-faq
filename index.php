f01ba53dcc2d7ac64037d2f635e756a67b65711b
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-02-24 14:34:23 +0100

    normalizzazione documenti index.php e style.css; struttura pagina

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- font-family: Poppins 400,700 -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+  <!-- foglio di stile principale -->
+  <link rel="stylesheet" href="css/style.css">
+  <title>Google FaQ</title>
+</head>
+<body>
+  <header class="header">
+    <div class="header__privacy-terms">
+
+    </div>
+    <div class="header__navbar">
+      <nav class="header__navbar__site-pages">
+
+      </nav>
+      <div class="header__navbar__google-account">
+
+      </div>
+    </div>
+  </header>
+  <main class="main">
+
+  </main>
+  <footer class="footer">
+    <nav class="footer__info">
+
+    </nav>
+    <div class="footer__language">
+
+    </div>
+  </footer>
+</body>
+</html>
