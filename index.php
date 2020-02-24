f512aad84fa8ad898d0b204cf2d80b48727088ca
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-02-24 15:17:39 +0100

    index.php: .header__privacy-terms

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
 <body>
   <header class="header">
     <div class="header__privacy-terms">
-
+      <img class="header__privacy-terms__google-logo" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_74x24dp.png" alt="Google">
+      <a class="header__privacy-terms__privacy" href="">Privacy e termini</a>
     </div>
     <div class="header__navbar">
       <nav class="header__navbar__site-pages">
